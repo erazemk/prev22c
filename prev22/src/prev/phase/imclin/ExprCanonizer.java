f1daf3f273993970344454530bb1f87047371aff
Erazem Kokot <mail@erazem.eu>
2022-04-17 16:52:50 +0200

    Add template for ExprCanonizer and StmtCanonizer

--- prev22/src/prev/phase/imclin/ExprCanonizer.java
+++ prev22/src/prev/phase/imclin/ExprCanonizer.java
@@ -1,6 +1,8 @@
 package prev.phase.imclin;
 
 import java.util.*;
+
+import prev.data.ast.tree.expr.*;
 import prev.data.mem.*;
 import prev.data.imc.code.expr.*;
 import prev.data.imc.code.stmt.*;
@@ -11,4 +13,35 @@ import prev.data.imc.visitor.*;
  */
 public class ExprCanonizer implements ImcVisitor<ImcExpr, Vector<ImcStmt>> {
 
+	public ImcExpr visit(ImcBINOP binOp, Vector<ImcStmt> stmts) {
+		return null;
+	}
+
+	public ImcExpr visit(ImcCALL call, Vector<ImcStmt> stmts) {
+		return null;
+	}
+
+	public ImcExpr visit(ImcCONST constant, Vector<ImcStmt> stmts) {
+		return null;
+	}
+
+	public ImcExpr visit(ImcMEM mem, Vector<ImcStmt> stmts) {
+		return null;
+	}
+
+	public ImcExpr visit(ImcNAME name, Vector<ImcStmt> stmts) {
+		return null;
+	}
+
+	public ImcExpr visit(ImcSEXPR sExpr, Vector<ImcStmt> stmts) {
+		return null;
+	}
+
+	public ImcExpr visit(ImcTEMP temp, Vector<ImcStmt> stmts) {
+		return null;
+	}
+
+	public ImcExpr visit(ImcUNOP unOp, Vector<ImcStmt> stmts) {
+		return null;
+	}
 }
--- prev22/src/prev/phase/imclin/StmtCanonizer.java
+++ prev22/src/prev/phase/imclin/StmtCanonizer.java
@@ -2,7 +2,6 @@ package prev.phase.imclin;
 
 import java.util.*;
 
-import prev.common.report.*;
 import prev.data.mem.*;
 import prev.data.imc.code.expr.*;
 import prev.data.imc.code.stmt.*;
@@ -13,4 +12,27 @@ import prev.data.imc.visitor.*;
  */
 public class StmtCanonizer implements ImcVisitor<Vector<ImcStmt>, Object> {
 
+	public Vector<ImcStmt> visit(ImcCJUMP cjump, Object obj) {
+		return null;
+	}
+
+	public Vector<ImcStmt> visit(ImcESTMT eStmt, Object obj) {
+		return null;
+	}
+
+	public Vector<ImcStmt> visit(ImcJUMP jump, Object obj) {
+		return null;
+	}
+
+	public Vector<ImcStmt> visit(ImcLABEL label, Object obj) {
+		return null;
+	}
+
+	public Vector<ImcStmt> visit(ImcMOVE move, Object obj) {
+		return null;
+	}
+
+	public Vector<ImcStmt> visit(ImcSTMTS stmts, Object obj) {
+		return null;
+	}
 }
