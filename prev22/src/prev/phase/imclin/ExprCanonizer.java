baea5ed2b8cbd0c9d8f8a794d157674e3d9ebf38
Erazem Kokot <mail@erazem.eu>
2022-04-18 11:54:11 +0200

    Implement ExprCanonizer

--- prev22/src/prev/phase/imclin/ExprCanonizer.java
+++ prev22/src/prev/phase/imclin/ExprCanonizer.java
@@ -14,34 +14,50 @@ import prev.data.imc.visitor.*;
 public class ExprCanonizer implements ImcVisitor<ImcExpr, Vector<ImcStmt>> {
 
 	public ImcExpr visit(ImcBINOP binOp, Vector<ImcStmt> stmts) {
-		return null;
+		// Store subexpression results
+		MemTemp temp1 = new MemTemp();
+		MemTemp temp2 = new MemTemp();
+
+		stmts.add(new ImcMOVE(new ImcTEMP(temp1), binOp.fstExpr.accept(this, stmts)));
+		stmts.add(new ImcMOVE(new ImcTEMP(temp2), binOp.sndExpr.accept(this, stmts)));
+
+		return new ImcBINOP(binOp.oper, new ImcTEMP(temp1), new ImcTEMP(temp2));
 	}
 
 	public ImcExpr visit(ImcCALL call, Vector<ImcStmt> stmts) {
-		return null;
+		Vector<ImcExpr> args = new Vector<>();
+
+		for (ImcExpr arg : call.args) {
+			MemTemp temp = new MemTemp();
+			stmts.add(new ImcMOVE(new ImcTEMP(temp), arg.accept(this, stmts)));
+			args.add(new ImcTEMP(temp));
+		}
+
+		return new ImcCALL(call.label, call.offs, args);
 	}
 
 	public ImcExpr visit(ImcCONST constant, Vector<ImcStmt> stmts) {
-		return null;
+		return constant;
 	}
 
 	public ImcExpr visit(ImcMEM mem, Vector<ImcStmt> stmts) {
-		return null;
+		return new ImcMEM(mem.addr.accept(this, stmts));
 	}
 
 	public ImcExpr visit(ImcNAME name, Vector<ImcStmt> stmts) {
-		return null;
+		return name;
 	}
 
 	public ImcExpr visit(ImcSEXPR sExpr, Vector<ImcStmt> stmts) {
-		return null;
+		stmts.addAll(sExpr.stmt.accept(new StmtCanonizer(), null));
+		return sExpr.expr.accept(this, stmts);
 	}
 
 	public ImcExpr visit(ImcTEMP temp, Vector<ImcStmt> stmts) {
-		return null;
+		return temp;
 	}
 
 	public ImcExpr visit(ImcUNOP unOp, Vector<ImcStmt> stmts) {
-		return null;
+		return new ImcUNOP(unOp.oper, unOp.subExpr.accept(this, stmts));
 	}
 }
