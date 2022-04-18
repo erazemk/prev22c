b5ab6cfdf99154ca256bd58c87544498b3e793b2
Erazem Kokot <mail@erazem.eu>
2022-04-18 21:39:05 +0200

    Fix interpreter to properly run strings

--- prev22/src/prev/phase/imclin/Interpreter.java
+++ prev22/src/prev/phase/imclin/Interpreter.java
@@ -53,9 +53,9 @@ public class Interpreter {
 			}
 			this.dataMemLabels.put(dataChunk.label, tempLD(HP, false));
 			if (dataChunk.init != null) {
-				for (int c = 0; c < dataChunk.init.length() - 2; c++)
-					memST(tempLD(HP, false) + 8 * c, (long) dataChunk.init.charAt(c + 1), false);
-				memST(tempLD(HP, false) + 8 * (dataChunk.init.length() - 2), 0L, false);
+				for (int c = 0; c < dataChunk.init.length(); c++)
+					memST(tempLD(HP, false) + 8 * c, (long) dataChunk.init.charAt(c), false);
+				memST(tempLD(HP, false) + 8 * (dataChunk.init.length()), 0L, false);
 			}
 			tempST(HP, tempLD(HP, false) + dataChunk.size, debug);
 		}
