d31fabb46ba3489fc4295dcdb763760de020dd0c
Erazem Kokot <mail@erazem.eu>
2022-04-18 12:07:09 +0200

    Implement some StmtCanonizer visitors

--- prev22/src/prev/phase/imclin/StmtCanonizer.java
+++ prev22/src/prev/phase/imclin/StmtCanonizer.java
@@ -13,19 +13,23 @@ import prev.data.imc.visitor.*;
 public class StmtCanonizer implements ImcVisitor<Vector<ImcStmt>, Object> {
 
 	public Vector<ImcStmt> visit(ImcCJUMP cjump, Object obj) {
-		return null;
+		Vector<ImcStmt> stmts = new Vector<>();
+		ImcExpr cond = cjump.cond.accept(new ExprCanonizer(), stmts);
+		return new Vector<>(List.of(new ImcCJUMP(cond, cjump.posLabel, cjump.negLabel)));
 	}
 
 	public Vector<ImcStmt> visit(ImcESTMT eStmt, Object obj) {
-		return null;
+		Vector<ImcStmt> stmts = new Vector<>();
+		ImcExpr expr = eStmt.expr.accept(new ExprCanonizer(), stmts);
+		return new Vector<>(List.of(new ImcESTMT(expr)));
 	}
 
 	public Vector<ImcStmt> visit(ImcJUMP jump, Object obj) {
-		return null;
+		return new Vector<>(List.of(jump));
 	}
 
 	public Vector<ImcStmt> visit(ImcLABEL label, Object obj) {
-		return null;
+		return new Vector<>(List.of(label));
 	}
 
 	public Vector<ImcStmt> visit(ImcMOVE move, Object obj) {
@@ -33,6 +37,12 @@ public class StmtCanonizer implements ImcVisitor<Vector<ImcStmt>, Object> {
 	}
 
 	public Vector<ImcStmt> visit(ImcSTMTS stmts, Object obj) {
-		return null;
+		Vector<ImcStmt> stmtVector = new Vector<>();
+
+		for (ImcStmt stmt : stmts.stmts) {
+			stmtVector.addAll(stmt.accept(this, null));
+		}
+
+		return stmtVector;
 	}
 }
