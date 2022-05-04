127e35d58826bc3416c716120a0b5683188b1ad8
Erazem Kokot <mail@erazem.eu>
2022-05-04 14:39:44 +0200

    Merge DN11

--- prev22/src/prev/Compiler.java
+++ prev22/src/prev/Compiler.java
@@ -14,6 +14,7 @@ import prev.phase.imcgen.*;
 import prev.phase.imclin.*;
 import prev.phase.asmgen.*;
 import prev.phase.livean.*;
+import prev.phase.regall.*;
 
 /**
  * The compiler.
@@ -29,7 +30,7 @@ public class Compiler {
 	public static boolean debug = false;
 
 	/** Values of command line arguments. */
-	private static HashMap<String, String> cmdLine = new HashMap<String, String>();
+	private static final HashMap<String, String> cmdLine = new HashMap<String, String>();
 
 	/**
 	 * Returns the value of a command line argument.
@@ -54,56 +55,62 @@ public class Compiler {
 			System.out.println(":-) This is PREV'22 compiler:");
 
 			// Scan the command line.
-			for (int argc = 0; argc < args.length; argc++) {
-				if (args[argc].startsWith("--")) {
+			for (String arg : args) {
+				if (arg.startsWith("--")) {
 					// Command-line switch.
-					if (args[argc].matches("--debug")) {
+					if (arg.matches("--debug")) {
 						debug = true;
 						continue;
 					}
-					if (args[argc].matches("--src-file-name=.*")) {
+					if (arg.matches("--src-file-name=.*")) {
 						if (cmdLine.get("--src-file-name") == null) {
-							cmdLine.put("--src-file-name", args[argc]);
+							cmdLine.put("--src-file-name", arg);
 							continue;
 						}
 					}
-					if (args[argc].matches("--dst-file-name=.*")) {
+					if (arg.matches("--dst-file-name=.*")) {
 						if (cmdLine.get("--dst-file-name") == null) {
-							cmdLine.put("--dst-file-name", args[argc]);
+							cmdLine.put("--dst-file-name", arg);
 							continue;
 						}
 					}
-					if (args[argc].matches("--target-phase=(" + phases + "|all)")) {
+					if (arg.matches("--target-phase=(" + phases + "|all)")) {
 						if (cmdLine.get("--target-phase") == null) {
-							cmdLine.put("--target-phase", args[argc].replaceFirst("^[^=]*=", ""));
+							cmdLine.put("--target-phase", arg.replaceFirst("^[^=]*=", ""));
 							continue;
 						}
 					}
-					if (args[argc].matches("--logged-phase=(" + phases + "|all)")) {
+					if (arg.matches("--logged-phase=(" + phases + "|all)")) {
 						if (cmdLine.get("--logged-phase") == null) {
-							cmdLine.put("--logged-phase", args[argc].replaceFirst("^[^=]*=", ""));
+							cmdLine.put("--logged-phase", arg.replaceFirst("^[^=]*=", ""));
 							continue;
 						}
 					}
-					if (args[argc].matches("--xml=.*")) {
+					if (arg.matches("--xml=.*")) {
 						if (cmdLine.get("--xml") == null) {
-							cmdLine.put("--xml", args[argc].replaceFirst("^[^=]*=", ""));
+							cmdLine.put("--xml", arg.replaceFirst("^[^=]*=", ""));
 							continue;
 						}
 					}
-					if (args[argc].matches("--xsl=.*")) {
+					if (arg.matches("--xsl=.*")) {
 						if (cmdLine.get("--xsl") == null) {
-							cmdLine.put("--xsl", args[argc].replaceFirst("^[^=]*=", ""));
+							cmdLine.put("--xsl", arg.replaceFirst("^[^=]*=", ""));
 							continue;
 						}
 					}
-					Report.warning("Command line argument '" + args[argc] + "' ignored.");
+					if (arg.matches("--nregs=[0-9]+")) {
+						if (cmdLine.get("--nregs") == null) {
+							cmdLine.put("--nregs", arg.replaceFirst("^[^=]*=", ""));
+							continue;
+						}
+					}
+					Report.warning("Command line argument '" + arg + "' ignored.");
 				} else {
 					// Source file name.
 					if (cmdLine.get("--src-file-name") == null) {
-						cmdLine.put("--src-file-name", args[argc]);
+						cmdLine.put("--src-file-name", arg);
 					} else {
-						Report.warning("Source file '" + args[argc] + "' ignored.");
+						Report.warning("Source file '" + arg + "' ignored.");
 					}
 				}
 			}
@@ -123,8 +130,7 @@ public class Compiler {
 				// Lexical analysis.
 				if (Compiler.cmdLineArgValue("--target-phase").equals("lexan"))
 					try (LexAn lexan = new LexAn()) {
-						while (lexan.lexer.nextToken().getType() != Token.EOF) {
-						}
+						while (lexan.lexer.nextToken().getType() != Token.EOF) {}
 						break;
 					}
 
@@ -185,8 +191,9 @@ public class Compiler {
 					Abstr.tree.accept(new ChunkGenerator(), null);
 					imclin.log();
 
-					/*Interpreter interpreter = new Interpreter(ImcLin.dataChunks(), ImcLin.codeChunks());
-					System.out.println("EXIT CODE: " + interpreter.run("_main"));*/
+					// Interpreter
+					//Interpreter interpreter = new Interpreter(ImcLin.dataChunks(), ImcLin.codeChunks());
+					//System.out.println("EXIT CODE: " + interpreter.run("_main"));
 				}
 				if (Compiler.cmdLineArgValue("--target-phase").equals("imclin"))
 					break;
@@ -207,6 +214,13 @@ public class Compiler {
 				if (Compiler.cmdLineArgValue("--target-phase").equals("livean"))
 					break;
 
+				// Register allocation.
+				try (RegAll regall = new RegAll(Integer.decode(cmdLine.get("--nregs")))) {
+					regall.allocate();
+					regall.log();
+				}
+				if (Compiler.cmdLineArgValue("--target-phase").equals("regall"))
+					break;
 			}
 
 			System.out.println(":-) Done.");
